181cefeea4c9e4885e922da725a226c4b76c6be1
wcoleda <wcoleda@6cb8db7d-f34b-0410-8f57-4f83c6281724>
2008-10-09 16:13:08 +0000

    remove commented out code and simplify
    git-svn-id: http://partcl.googlecode.com/svn/trunk@55 6cb8db7d-f34b-0410-8f57-4f83c6281724

--- examples/power.tcl
+++ examples/power.tcl
@@ -4,17 +4,12 @@ exec ../../../parrot ../tcl.pbc "$0" "$@"
 
 proc power {number power} {
   set val 1
-  #puts "here"
   while {$power != 0} {
     set val [expr $val * $number]
     incr power -1
-    #puts $val
   }
   return $val
 }
 
-power 10 2
-set a [power 10 2]
-puts "10**2 is $a"
-set a [power 2 10]
-puts "2**10 is $a"
+puts "10**2 is [power 10 2]"
+puts "2**10 is [power 2 10]"
